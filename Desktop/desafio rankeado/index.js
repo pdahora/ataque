ec81054b26708ccd52e95953d60a8296074edfbd
pdahora <pdahora@hotmail.com>
2023-09-30 13:47:28 -0300

    Mais um projeto

--- Desktop/desafio rankeado/index.js
+++ Desktop/desafio rankeado/index.js
@@ -1,30 +1,42 @@
 
-function nivelVitorias(vitorias,derrota) {
-    const saldoVitorias = vitorias - derrotas;
-    
+function nivelVitorias(vitorias) {
     if (vitorias < 10) {
-        return `O Herói tem ${vitorias} vitórias e está no nível de Bronze}`
-    }
-    else if (vitorias >= 10 && vitorias <= 20) {
-        return `O Herói tem ${vitorias} vitórias e está no nível de Prata}`;
-    }
-    else if (vitorias >= 21 && vitorias <= 50) {
+        return `O Herói tem ${vitorias} vitórias e está no nível de Bronze`;
+    } else if (vitorias >= 10 && vitorias <= 20) {
+        return `O Herói tem ${vitorias} vitórias e está no nível de Prata.`;
+
+    } else if (vitorias >= 21 && vitorias <= 50) {
         return `O Herói tem ${vitorias} vitórias e está no nível de Ouro}`;
-    }
-    else if (vitorias >= 51 && vitorias <= 80) {
+
+    } else if (vitorias >= 51 && vitorias <= 80) {
         return `O Herói tem ${vitorias} vitórias e está no nível de Diamante}`;
-    }
-    else if (vitorias >= 81 && vitorias <= 90) {
+
+    } else if (vitorias >= 81 && vitorias <= 90) {
         return `O Herói tem ${vitorias} vitórias e está no nível de Super}`;
-    }
-    else if (vitorias >= 91 && vitorias <= 100) {
-        return `O Herói tem ${vitorias} vitórias e está no nível de Lendário}`;
-    } 
-    else {
+    } else {
         return `O Herói tem ${vitorias} vitórias e está no nível de Imortal}`;
-    }   
+    }
 }
+
+function nivelDerrota(derrotas) {
+    if (derrotas < 10) {
+        return `O Herói tem ${derrotas} derrotas e está no nível de Bronze`;
+    } else if (derrotas >= 10 && derrotas <= 20) {
+        return "Prata";
+    } else {
+        return "Invencível";
+    }
+}
+
 let derrotas = 2;
-let vitorias = 105;
-let resultado = nivelVitorias(vitorias);
-console.log(resultado)
+let vitorias = 10;
+
+let nivelVitoriaTexto = nivelVitorias(vitorias);
+let nivelDerrotaTexto = nivelDerrota(derrotas);
+
+console.log(nivelVitoriaTexto);
+console.log(nivelDerrotaTexto);
+
+let resultado = vitorias - derrotas;
+
+console.log("diferanca de vitórias:", resultado);
